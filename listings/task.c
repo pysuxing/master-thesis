5aa560155bbdaaf55e608540e9b916ef2abbb2c2
SuXing <pysuxing@gmail.com>
2013-11-05 17:03:17 +0800

    第四章结构大调整，将运行时系统作为第五章。基本完成了第四章写作。

--- listings/task.c
+++ listings/task.c
@@ -1,11 +1,11 @@
-struct {
-    int input_number;           /* number of input data */
-    void** input;               /* pointers to input data */
-    size_t* input_length;       /* pointers to input data length */
-    int* input_type;            /* pointers to input element type */
-    void* out;                  /* output data */
-    size_t outlen;              /* output length */
-    int outtype;                /* outtype element type */
-    vector_primitive_t vp_type; /* vector primitive type */
-    void* vp;                   /* vector primitive struct */
-}
+typedef struct {
+    int in_number;           /* number of input data */
+    void** in;               /* pointers to input data */
+    size_t* in_length;       /* pointers to input data length */
+    int* in_type;            /* pointers to input element type */
+    void* out;               /* output data */
+    size_t outlen;           /* output length */
+    int outtype;             /* outtype element type */
+    vp_t vp_type;            /* vector primitive type */
+    void* vp;                /* vector primitive struct */
+} task_t;
